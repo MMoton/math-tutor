a924ab73c2a15be88c6e75b4d6403712db6692fc
MMoton <mayam03@icloud.com>
2026-05-10 18:30:16 -0400

    Initial Commit

---
+++ math tutor/main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  math tutor
+//
+//  Created by Maya Moton on 4/27/26.
+//
+
+#include <iostream>
+#include <cstdlib>   
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    
+    srand((unsigned)time(0));
+
+    // Generate two random numbers
+    int num1 = rand() % 1000;
+    int num2 = rand() % 1000;
+
+    // Display the problem
+    cout << "Solve the problem:\n\n";
+    cout << "  " << num1 << endl;
+    cout << "+ " << num2 << endl;
+
+    
+    cout << "\nPress Enter to see the answer...";
+    cin.ignore();  // wait for Enter
+
+    // Display the answer
+    cout << "\n  " << num1 << endl;
+    cout << "+ " << num2 << endl;
+    cout << "-----" << endl;
+    cout << "  " << (num1 + num2) << endl;
+
+    return 0;
+}
+
